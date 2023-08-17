e23f8d7026ac4e2bb8629bfb2622e8dc3cecfbf0
narakushi <taina.santana1305@gmail.com>
2023-08-17 05:34:36 -0300

    operador ternario e objeto date

---
+++ aula43(08)-operação-ternária/script.js
@@ -0,0 +1,13 @@
+const pontosUsuario = 1000;
+const nivelUsuario = (pontosUsuario >= 1000) ? 'usuário VIP' : 'usuário normal';
+
+console.log(nivelUsuario);
+
+/**
+ * substitui
+ * if(pontosUsuario >= 1000){
+ *  console.log('usuario VIP');
+ * }else{
+ *  console.log('usuario normal');
+ * }
+ */
