8f9ae9dbd76d6b41590005e27e045aa7d98c6ab9
narakushi <taina.santana1305@gmail.com>
2023-08-21 21:11:18 -0300

    atribuicao via desestruturação

---
+++ aula48(13)-atribuição-via-desestruturacao-arrays/script.js
@@ -0,0 +1,14 @@
+// let a = 'A';
+// let b = 'B';
+// let c = 'C';
+
+
+// [a, b, c] = [1, 2, 3];
+                
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+const [primeiro, segundo, ...resto] = numeros; //pegando o primeiro e segundo valor, dps o resto
+
+console.log(primeiro, segundo, resto)
+
+
