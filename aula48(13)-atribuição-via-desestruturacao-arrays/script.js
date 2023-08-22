f93329bae55b8fca6e9702f1bf3a531af0c52557
narakushi <taina.santana1305@gmail.com>
2023-08-22 05:45:34 -0300

    modificacao

--- aula48(13)-atribuição-via-desestruturacao-arrays/script.js
+++ aula48(13)-atribuição-via-desestruturacao-arrays/script.js
@@ -4,11 +4,21 @@
 
 
 // [a, b, c] = [1, 2, 3];
-                
-const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-const [primeiro, segundo, ...resto] = numeros; //pegando o primeiro e segundo valor, dps o resto
+// const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-console.log(primeiro, segundo, resto)
+// const [primeiro, segundo, ...resto] = numeros; //pegando o primeiro e segundo valor, dps o resto
 
+// console.log(primeiro, segundo, resto)
+
+                              
+const numbers = [[1, 2, 3], [4, 5, 6]];
+
+const [lista1, lista2] = numbers;
+
+console.log(lista1, lista2);
+
+//acesso
+//pegando o primeiro elemento do segundo array 
+console.log(numbers[1][0]);
 
