7d9d639d6189f5af53d615ae5e11bdc54c380f01
Tainá Santana <taina.santana1305@gmail.com>
2024-03-24 21:08:11 -0300

    relembrando trabalhar com nodelist e captação de estilos por meio de js

--- aula57(22)-exercicio-nodelist/js/script.js
+++ aula57(22)-exercicio-nodelist/js/script.js
@@ -1,13 +1,10 @@
 const paragrafos = document.querySelector('.paragrafos');
-const estilosBody = getComputedStyle(document.body);
-
-console.log(estilosBody);
-const backgroundColorBody = estilosBody.backgroundColor;
-
 const ps = paragrafos.querySelectorAll('p');
 
-for(let p of ps){
-    p.style.backgroundColor = backgroundColorBody;
-    p.style.marginTop = '2rem';
-    p.style.color = '#ff4345';
+const estilosBody = getComputedStyle(document.body);
+const estiloBg = estilosBody.backgroundColor;
+console.log(estiloBg);
+
+for (let p of ps) {
+    p.style.background = estiloBg;
 }
