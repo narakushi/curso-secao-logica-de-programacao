f8ab513782580afaa3027fd7edf16266c6fdd7e7
Tainá Santana <taina.santana1305@gmail.com>
2024-03-24 23:12:02 -0300

    trablahando com numeros randomicos em um while

--- aula58(23)-wihile-do-while/script.js
+++ aula58(23)-wihile-do-while/script.js
@@ -1,7 +1,14 @@
-let i = 0; 
-
-
-while (i <= 10) {
-    console.log(i);
-    i++;
+function random(min, max){
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
 }
+
+let rand = random(1, 51);
+
+
+
+while((rand != 50)){
+    rand = random(1, 51);
+}
+
+console.log(rand);
