f2a1a89dae440e3370db57ef242beea68a623cb3
Tainá Santana <taina.santana1305@gmail.com>
2024-04-29 23:10:41 -0300

    trabalhando com arrow function e operadores ternários

---
+++ aula60(25)-exercicio-logica-01/script.js
@@ -0,0 +1,10 @@
+//Escreva uma função que recebe dois numeros e retorne o maior deles
+
+const maior = (num1, num2) => {
+    return num1 > num2 ? num1 : num2;
+}
+
+const num1 = 1;
+const num2 = 88;
+
+console.log(maior(num1, num2));
