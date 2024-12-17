14544339452569f99ca198369938d2f02192ac42
narakushi <taina.santana1305@gmail.com>
2024-12-17 12:31:58 -0300

    revisando tratamento de erros

--- aula63(28)-tratando-erros(try-catch-throw)/script.js
+++ aula63(28)-tratando-erros(try-catch-throw)/script.js
@@ -1,6 +1,17 @@
+/** utilização do try catch */
+/*try {
+  console.log(naoExisto);
+} catch(err) {
+  console.log('nãoExisto não existe.');
+  console.log(err);
+}*/
+
+
+/** lançando nossos próprios erros */
+
 function soma(x, y) {
   if (typeof x !== "number" || typeof y !== "number") {
-    throw new SyntaxError("x e y precisam ser números");
+    throw new TypeError('X e y precisam ser números');
   }
 
   return x + y;
@@ -8,8 +19,9 @@ function soma(x, y) {
 
 try {
   console.log(soma(1, 2));
- // console.log(soma("1", 2));
-} catch (error) {
-  //console.log(error);
-  console.log("Alguma coisa mais amigável para o usuário")
+  console.log(soma('1', 2));
+}
+catch(error){
+  console.log(error);
+  console.log('Alguma coisa mais amigável')
 }
--- aula64(29)-tratando-lancanco-erros(try-catch-finally))/script.js
+++ aula64(29)-tratando-lancanco-erros(try-catch-finally))/script.js
@@ -1,27 +1,52 @@
-function retornaHora(data) {
-  if (data && !(data instanceof Date)) {
-    throw new TypeError("esperando intância de Date.");
+/** executando tratamento de erros com bloco finally que sempre será executato e identando trys catch */
+/*
+try {
+  // É executado quando não há erros
+  // console.log(a);
+  console.log('Abri um arquivo');
+  console.log('Manipulei o arquivo e gerou erro');
+  console.log('Fechei o arquivo');
+
+  try {
+    console.log(b);
+  }catch(e) {
+    console.log('Erro interno');
+  } finally {
+    console.log('Também sou FINALLY')
   }
 
-  if (!data) {
+} catch(e) {
+  // É executado quando há erros
+  console.log('Trantando o erro');
+} finally {
+  // Sempre é excutado
+  console.log('FINALLY: Eu sempre sou executado');
+}*/
+
+function retornaHora(data) {
+  if(data && !(data instanceof Date)){
+    throw new TypeError('Esperando intância de Date.');
+  }
+
+  if(!data){
     data = new Date();
   }
 
-  return data.toLocaleTimeString("pt-BR", {
-    hour: "2-digit", //para retornar com 2 digitos
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  });
+  return data.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  })
 }
 
 try {
-  const data = new Date("01-01-1970 12:58:12");
+  const data = new Date('01-01-1970 12:58:12')
   const hora = retornaHora();
-  console.log(hora);
-} catch (e) {
-    //tratar erro
-  //console.log(e);
+  console.log(hora);  
+}catch(e){
+  // Tratar erro
+  console.log(e);
 } finally {
-  console.log("Tenha um bom dia");
+  console.log('Tenha um bom dia')
 }
