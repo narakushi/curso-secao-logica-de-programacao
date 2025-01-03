c9b0120a767dc76f4f336ce7119f50282ad910c4
narakushi <taina.santana1305@gmail.com>
2025-01-03 13:56:31 -0300

    revisando aula 55 sobre fo in

--- aula55(20)-for-in-estrutura-de-repeticao/script.js
+++ aula55(20)-for-in-estrutura-de-repeticao/script.js
@@ -1,13 +1,29 @@
+// for in -> lê índices ou chaves do objeto
+
+/*
+const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let indice in frutas) {
+    console.log(frutas[indice]);
+}
+*/
+
+/*
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}*/
+
+
 const pessoa = {
-    nome: 'Taina',
-    sobrenome: 'Jilnin',
-    idade: 23
-};
+    nome: 'Anais',
+    sobrenome: 'Jhonson',
+    idade: 34
+}
 
-//formas de acessar um objeto
-
+// formas de acessar o valor de uma chave em um objeto
+//console.log(pessoa.nome);
 //console.log(pessoa['nome']);
 
-for(let chave in pessoa){
+for (let chave in pessoa) {
     console.log(chave, pessoa[chave]);
 }
